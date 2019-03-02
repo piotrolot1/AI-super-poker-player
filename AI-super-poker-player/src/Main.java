b0b3d77aac7e406bef91fbba929b06c327193e84
piotrolot1 <piotrolot1@gmail.com>
2019-03-02 19:34:34 +0100

    Delete tests

--- AI-super-poker-player/src/Main.java
+++ AI-super-poker-player/src/Main.java
@@ -2,11 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Deck deck = Deck.getDeck();
-		deck.shuffling();
-		System.out.println("---");
-		deck.showDeck();
+		// Implementation the game
 	}
 
 }
